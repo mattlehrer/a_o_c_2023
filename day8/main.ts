cf4b0f79ce626597dcf8ceb8e15999d6b3e1c724
Matt Lehrer <lehrerm@gmail.com>
2023-12-08 16:45:03 +0100

    day 8 part 2

--- day8/main.ts
+++ day8/main.ts
@@ -1,12 +1,11 @@
 if (import.meta.main) {
-	const input = await Deno.readTextFile('./day8/input');
-
-	const lines = input.trim().split('\n');
-
 	// deno-lint-ignore no-unused-labels
 	part1: {
+		const input = await Deno.readTextFile('./day8/sample');
+	
+		const lines = input.trim().split('\n');
 		const instructions = lines[0].split('');
-		
+	
 		const map: Record<string, { left: string, right: string }> = {};
 		for (let i = 2; i < lines.length; i++) {
 			const line = lines[i].split(' = ');
@@ -24,12 +23,54 @@ if (import.meta.main) {
 			}
 			steps++;
 		}
-		console.log({ steps });
-
+		console.log('part 1', { steps });
 	}
 	
 	// deno-lint-ignore no-unused-labels
-	// part2: {
+	part2: {
+		const input = await Deno.readTextFile('./day8/input');
 	
-	// }	
+		const lines = input.trim().split('\n');
+		const instructions = lines[0].split('');
+	
+		const map: Record<string, { left: string, right: string }> = {};
+		for (let i = 2; i < lines.length; i++) {
+			const line = lines[i].split(' = ');
+			const [left, right] = line[1].replace('(', '').replace(')', '').split(', ');
+			map[line[0]] = { left, right };
+		}
+
+		const starters = Object.keys(map).filter(key => key.endsWith('A'))
+		const stepNumbers: number[] = [];
+		for (const current of starters) {
+			let c = current;
+			let steps = 0;
+			while (!c.endsWith('Z')) {
+				const instruction = instructions[steps % instructions.length];
+				if (instruction === 'L') {
+					c = map[c].left;
+				} else if (instruction === 'R') {
+					c = map[c].right;
+				}
+				steps++;
+			}
+			stepNumbers.push(steps);
+		}
+		console.log({ stepNumbers})
+		// find lowest common multiple of all step numbers
+		let steps = stepNumbers[0];
+		for (let i = 1; i < stepNumbers.length; i++) {
+			let a = steps;
+			let b = stepNumbers[i];
+			while (a !== b) {
+				if (a < b) {
+					a += steps;
+				} else {
+					b += stepNumbers[i];
+				}
+			}
+			steps = a;
+		}
+		console.log('part 2', { steps });
+	}
 }
