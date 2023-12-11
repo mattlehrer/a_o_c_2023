86cb16e357294388282bf8ee875f5c4a58228525
Matt Lehrer <lehrerm@gmail.com>
2023-12-11 09:05:16 +0100

    day11 part 1

---
+++ day11/main.ts
@@ -0,0 +1,64 @@
+// deno-lint-ignore-file no-unused-labels
+
+if (import.meta.main) {
+	const input = await Deno.readTextFile('./day11/input');
+
+	const lines = input
+		.trim()
+		.split('\n')
+		.map((line) => line.split(''));
+
+	part1: {
+		const expandedUniverse: string[][] = [];
+		for (let y = 0; y < lines.length; y++) {
+			const line = lines[y];
+			if (line.every((c) => c === '.')) {
+				expandedUniverse.push(line, line);
+			} else {
+				expandedUniverse.push(line);
+			}
+		}
+		const addColumns: number[] = [];
+		for (let x = 0; x < expandedUniverse[0].length; x++) {
+			let hasGalaxy = false;
+			for (let y = 0; y < expandedUniverse.length; y++) {
+				if (expandedUniverse[y][x] === '#') {
+					hasGalaxy = true;
+					break;
+				}
+			}
+			if (!hasGalaxy) {
+				addColumns.push(x);
+			}
+		}
+
+		addColumns.forEach((x, i) => {
+			for (let y = 0; y < expandedUniverse.length; y++) {
+				expandedUniverse[y] = [expandedUniverse[y].slice(0, x + i), '.', expandedUniverse[y].slice(x + i)].flat();
+			}
+		});
+
+		const galaxies: [number, number][] = [];
+		for (let y = 0; y < expandedUniverse.length; y++) {
+			const line = expandedUniverse[y];
+			for (let x = 0; x < line.length; x++) {
+				if (line[x] === '#') {
+					galaxies.push([x, y]);
+				}
+			}
+		}
+
+		let totalDistance = 0;
+
+		for (let i = 0; i < galaxies.length - 1; i++) {
+			for (let j = i + 1; j < galaxies.length; j++) {
+				const [x1, y1] = galaxies[i];
+				const [x2, y2] = galaxies[j];
+				const distance = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+				totalDistance += distance;
+			}
+		}
+
+		console.log('part 1: ', totalDistance);
+	}
+}
